2ac6daa13a0f9dddd40288ce269b17da3e3a4e19
Janosch Machowinski <janosch.machowinski@dfki.de>
2010-06-08 13:22:58 +0200

    init GPIO initstruct sane, and set GPIO speed

--- firmware/can.c
+++ firmware/can.c
@@ -20,10 +20,12 @@ void CAN_Configuration(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     
     GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_StructInit(&GPIO_InitStructure);
 
 	//Configure CAN pin: RX 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     //Configure CAN pin: TX 
