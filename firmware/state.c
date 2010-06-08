a4a7bf9ae5273e2ae72f06468edcdb582ec2e3e7
Janosch Machowinski <janosch.machowinski@dfki.de>
2010-06-08 13:36:39 +0200

    make debug message more human readable

--- firmware/state.c
+++ firmware/state.c
@@ -44,6 +44,40 @@ void initStateStruct(volatile struct ControllerState *cs) {
 
 void printStateDebug(volatile struct ControllerState* cs)
 {
+    char *ctrl_s = "";
+    char *int_state_s = "";
+    switch(cs->controllMode) {
+	case CONTROLLER_MODE_PWM:
+	    ctrl_s = "PWM";
+	    break;
+	case CONTROLLER_MODE_POSITION:
+	    ctrl_s = "POSITION";
+	    break;
+	case CONTROLLER_MODE_SPEED:
+	    ctrl_s = "SPEED";
+	    break;
+    }
+    switch(cs->internalState) {
+	case STATE_CONFIG1_RECEIVED:
+	    int_state_s = "CONF1";
+	    break;
+	case STATE_CONFIG2_RECEIVED:
+	    int_state_s = "CONF2";
+	    break;
+	case STATE_CONFIGURED:
+	    int_state_s = "CONFIGURED";
+	    break;
+	case STATE_ERROR:
+	    int_state_s = "ERROR";
+	    break;
+	case STATE_GOT_TARGET_VAL:
+	    int_state_s = "GOT_TARGET";
+	    break;
+	case STATE_UNCONFIGURED:
+	    int_state_s = "UNCONFIGURED";
+	    break;
+    }
+    printf("ControllMode: %s ,internal State: %s ,targetVal : %li ,openloop:%hi ,backIndo %hi ,pwmstep %hu \n", ctrl_s, int_state_s, cs->targetValue, cs->useOpenLoop, cs->useBackInduction, cs->pwmStepPerMillisecond);    
 }
 
 u8 inErrorState() {
