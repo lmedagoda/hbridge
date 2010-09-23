7314c6cbfd1852acac4674bbbe5b44ef3beff8a8
Janosch Machowinski <janosch.machowinski@dfki.de>
2010-09-23 11:25:49 +0200

    use internal pull up for external encoder signal lines

--- firmware/encoder.c
+++ firmware/encoder.c
@@ -245,7 +245,7 @@ void encoderInitExtern() {
     //Configure GPIO pins: AIN BIN and Zero
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOB, &GPIO_InitStructure);    
 
     GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource12);
