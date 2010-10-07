3781c1a5836551aad633d01eb5e2889daeb7ec10
Martin Fritsche <fritsche@fritsche.dfki.uni-bremen.de>
2010-10-07 16:28:09 +0200

    accept all can packages that match the board id

--- firmware/can.c
+++ firmware/can.c
@@ -88,7 +88,7 @@ void CAN_ConfigureFilters(enum hostIDs boardNr) {
     CAN_FilterInitStructure.CAN_FilterIdLow=PACKET_ID_SET_VALUE58<<5;
     CAN_FilterInitStructure.CAN_FilterMaskIdHigh=PACKET_ID_SET_MODE58<<5;      
   }
-  CAN_FilterInitStructure.CAN_FilterMaskIdLow=(PACKET_ID_ENCODER_CONFIG + boardNr)<<5;
+  CAN_FilterInitStructure.CAN_FilterMaskIdLow=0;
   CAN_FilterInitStructure.CAN_FilterFIFOAssignment=CAN_FIFO0;
   CAN_FilterInitStructure.CAN_FilterActivation=ENABLE;
   CAN_FilterInit(&CAN_FilterInitStructure);
@@ -97,10 +97,12 @@ void CAN_ConfigureFilters(enum hostIDs boardNr) {
   CAN_FilterInitStructure.CAN_FilterNumber=1;
   CAN_FilterInitStructure.CAN_FilterMode=CAN_FilterMode_IdList;
   CAN_FilterInitStructure.CAN_FilterScale=CAN_FilterScale_16bit;
-  CAN_FilterInitStructure.CAN_FilterIdHigh=(PACKET_ID_SET_CONFIGURE + boardNr)<<5;
-  CAN_FilterInitStructure.CAN_FilterIdLow=(PACKET_ID_SET_CONFIGURE2 + boardNr)<<5;
-  CAN_FilterInitStructure.CAN_FilterMaskIdHigh=(PACKET_ID_SET_PID_POS + boardNr)<<5;
-  CAN_FilterInitStructure.CAN_FilterMaskIdLow=(PACKET_ID_SET_PID_SPEED + boardNr)<<5;;
+  //let pass every message, that matches the boardNr
+  CAN_FilterInitStructure.CAN_FilterIdHigh=(boardNr)<<5;
+  CAN_FilterInitStructure.CAN_FilterIdLow=0;
+  //0x1E0 is the mask that matches all RECEIVER_IDs
+  CAN_FilterInitStructure.CAN_FilterMaskIdHigh=(0x1E0) << 5;
+  CAN_FilterInitStructure.CAN_FilterMaskIdLow=0;
   CAN_FilterInitStructure.CAN_FilterFIFOAssignment=CAN_FIFO1;
   CAN_FilterInitStructure.CAN_FilterActivation=ENABLE;
   CAN_FilterInit(&CAN_FilterInitStructure);
