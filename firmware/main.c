e4620190a5a02792893dd3355769455d9e382135
Jan Hahlbeck <jan.hahlbeck@dfki.de>
2010-12-02 17:19:16 +0100

    remove the interrupts for TIM2 and ADC1_2, because they are not needed anymore

--- firmware/main.c
+++ firmware/main.c
@@ -557,20 +557,6 @@ void NVIC_Configuration(void)
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
 
-  //Configure and enable TIM2 interrupt
-  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQChannel;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-
-  /* Configure and enable ADC interrupt */
-  NVIC_InitStructure.NVIC_IRQChannel = ADC1_2_IRQChannel;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-
   /* Configure and enable ADC interrupt */
   NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel1_IRQChannel;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
