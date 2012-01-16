ada5a226e139967551b2eccc193f2f6c4621fe40
Janosch Machowinski <janosch.machowinski@dfki.de>
2012-01-16 14:50:33 +0100

    added dummy implementation of PCBv2 current measurement
    This implementation just returns 0 atm

---
+++ firmware/current_measurementv2.c
@@ -0,0 +1,19 @@
+#include "inc/stm32f10x_type.h"
+
+
+void requestNewADCValues()
+{
+}
+
+void waitForNewADCValues()
+{
+}
+
+void currentMeasurementInit()
+{
+}
+
+u32 calculateCurrent()
+{
+    return 0;
+}
