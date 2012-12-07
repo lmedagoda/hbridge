072e47a57174011ba129f0801d84fca169e2feb6
Janosch Machowinski <janosch.machowinski@dfki.de>
2012-12-07 20:18:45 +0100

    Readded pwm_test testprogramm

--- test/pwm_test.cpp
+++ test/pwm_test.cpp
@@ -2,57 +2,61 @@
 #include <strings.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include <arpa/inet.h>
-
 #include <canbus.hh>
-#include "../HBridgeDriver.hpp"
-#include "../protocol.hpp"
-#include "../HBridge.hpp"
+#include "../src/CanBusInterface.hpp"
+#include "../src/Reader.hpp"
+#include "../src/Writer.hpp"
+#include "../src/Controller.hpp"
 
-canbus::Driver *driver = 0;
-hbridge::Driver hbd;
+using namespace hbridge;
+
+class CanDriver: public CanBusInterface
+{
+    canbus::Driver *driver;
+public:
+    CanDriver(const std::string &dev)
+    {
+	driver = canbus::openCanDevice(dev);	
+    };
+    
+    virtual bool readCanMsg(canbus::Message& msg)
+    {
+        if(driver->getPendingMessagesCount() > 0)
+	{
+	    msg = driver->read();
+	    return true;
+	}
+	
+	return false;
+    }
+    
+    virtual bool sendCanMsg(const canbus::Message& msg)
+    {
+	driver->write(msg);
+	return true;
+    }
+};
+
 int hbridge_id;
+int error;
+bool configured;
 
-const double ticksPerTurnIntern = (4096 * 16224) /245.0;
-const unsigned int ticksPerTurnExtern = 2000;
-
-void initDriver() {
-    if(driver)
-      return;
-
-    const char *can_device = "can0";
-
-    std::cerr << "Trying to open CAN device " << can_device << std::endl;
-#if CANBUS_VERSION >= 101
-    driver = canbus::openCanDevice(can_device);
-#else
-    driver = new canbus::Driver();
-    driver->open(can_device);
-#endif
-
-}
-
-hbridge::Configuration getDefaultConfig() {
-    hbridge::Configuration config;
-    bzero(&config, sizeof(hbridge::Configuration));
-
-    config.maxPWM = 1800;
-    config.maxSpeed = 300;
-    config.openCircuit = 1;
-    config.maxMotorTemp = 60;
-    config.maxMotorTempCount = 200;
-    config.maxBoardTemp = 60;
-    config.maxBoardTempCount = 200;
-    config.timeout = 50;
-    config.maxCurrent = 10000;
-    config.maxCurrentCount = 250;
-    config.pwmStepPerMs = 200;
-    config.externalTempSensor = 1;
-
-    return config;
-}
-
+class DummyCallback : public Reader::CallbackInterface
+{
+    virtual void configurationError()
+    {
+	std::cout << "Error while configuring " << std::endl;
+	error = 1;
+	exit(EXIT_FAILURE);
+    };
+    
+    virtual void configureDone()
+    {
+	configured = true;
+	std::cout << "Configuration done " << std::endl;
+    }
+};
 
 int main(int argc, char *argv[]) {
 
@@ -60,11 +64,10 @@ int main(int argc, char *argv[]) {
     timeval start, tick;
     gettimeofday(&start, 0);
 
-    double pwm_in = 0;
     double pwm = 0;
 
     if (argc == 3) {
-	pwm_in = strtod(argv[2],NULL);
+	double pwm_in = strtod(argv[2],NULL);
 	hbridge_id = strtol(argv[1],NULL,10);
 
 	if (pwm_in >= -1.0 && pwm_in <= 1.0) {    
@@ -80,86 +83,59 @@ int main(int argc, char *argv[]) {
 	exit(0);
     }
 
-    initDriver();
-    driver->setWriteTimeout(500);
-    canbus::Message msg;
+    hbridge::Protocol *proto = new Protocol(new CanDriver("can0"));
 
-    std::cerr << "Configuring Encoders " << (hbridge_id +1) << std::endl;
-    hbridge::EncoderConfiguration encConfInt(ticksPerTurnIntern, hbridge::ENCODER_QUADRATURE);
-    hbridge::EncoderConfiguration encConfExt(ticksPerTurnExtern, hbridge::ENCODER_QUADRATURE_WITH_ZERO);
+    proto->setSendTimeout(base::Time::fromMilliseconds(150));
     
-    canbus::Message encConfMsg = hbd.setInternalEncoderConfiguration(hbridge_id, encConfInt);
-    driver->write(encConfMsg);
-
-    encConfMsg = hbd.setExternalEncoderConfiguration(hbridge_id, encConfExt);
-    driver->write(encConfMsg);    
-
-    //now reconfigure
-    hbridge::Configuration config = getDefaultConfig();
-    hbridge::MessagePair config_msgs;
-
-    std::cerr << "Configuring hbridge " << (hbridge_id+1) << std::endl;
-    config_msgs = hbd.setConfiguration(hbridge_id, config);
-    driver->write(config_msgs.first);
-    driver->write(config_msgs.second);
-
-    //wait until we are shure everything is send on the bus and the hbridge processed it
-    //not this time is so high because of usb-can adapters
-    usleep(50000);
+    hbridge::HbridgeHandle *handle = proto->getHbridgeHandle(hbridge_id);
     
-    //clear canbus to get rid of old error messages
-    while(driver->getPendingMessagesCount() > 0) 
+    PWMController pwmCtrl(handle);
+    SpeedPIDController speedCtrl(handle);
+    PosPIDController posCtrl(handle);
+    
+    MotorConfiguration conf;
+    
+    Reader *reader = handle->getReader();
+    Writer *writer = handle->getWriter();
+    
+    bool configured = false;
+    
+    reader->setCallbacks(new DummyCallback());
+    reader->setConfiguration(conf);
+    reader->startConfigure();
+    
+    int cnt = 0;
+    while(!configured)
     {
-	msg = driver->read() ;
+	proto->processIncommingPackages();
+	proto->processSendQueues();
+	if(cnt == 500)
+	{
+	    std::cout << "Waiting for configuration to be done" << std::endl;
+	    cnt = 0;
+	}
+	cnt++;
+	usleep(1000);
     }
-
-    std::cerr << "Set drive mode " << std::endl;
-    msg = hbd.setDriveMode(hbridge::BOARDS_14, base::actuators::DM_PWM);
-    driver->write(msg);
     
-    int msg_cnt = 0;
-    unsigned int average = 0;
-    unsigned int average_cnt = 0;
+    while(!error)
+    {    
+	proto->processIncommingPackages();
+// 	std::cout << "SQ" << std::endl;
+	proto->processSendQueues();	
 
-    std::cerr << "Start hbridge... " << std::endl;
-
-    while(1) 
-    {
-        canbus::Message msg = hbd.setTargetValues(hbridge::BOARDS_14, pwm, pwm, pwm, pwm);
-
-        driver->write(msg);
-        //usleep(10000); // 10ms
-        usleep(1000); // 1ms
-
-        bool gotmsg = false;
-        while(driver->getPendingMessagesCount() > 0) {
-          msg = driver->read() ;
-          hbd.updateFromCAN(msg);
-          gotmsg = true;
-        }
-
-        if(gotmsg) {
-            msg_cnt++;
-            hbridge::BoardState state = hbd.getState(hbridge_id);
-            gettimeofday(&tick, 0);
-            long diff = (tick.tv_sec*1000000 + tick.tv_usec) - (start.tv_sec*1000000 + start.tv_usec);
-            std::cout << diff/1000 << " Current : " << state.current << " TicksInt: " << state.position << " TicksExt: " << state.positionExtern << " boardTemp " << state.temperature << " motorTemp " << state.motorTemperature << std::endl;
-            average += state.current;
-            average_cnt++;
-            if (msg_cnt == 20) {
-              //std::cout  << diff/1000 << " " << (average/average_cnt) << std::endl;
-              average = 0;
-              average_cnt = 0;
-              msg_cnt = 0;
-           }
-           
-           if(state.error.hasError())
-	   {
-		state.error.printError();
-		exit(1);
-	   }
-           
-        }
-     }
+	if(reader->isWritable())
+	{
+	    writer->setActiveController(&pwmCtrl);
+	    writer->setTargetValue(pwm);
+// 	    std::cout << "SM" << std::endl;
+	}
+	proto->sendSharedMessages();
+	
+	cnt++;
+	
+	if(cnt > 15)
+	    break;
+    }    
 }
 
