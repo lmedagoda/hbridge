2c2be35f923f50d19f2dd012e95fef2e9694c769
Niklas Pech <npech@tzi.de>
2013-02-08 11:49:13 +0100

    Clean some debugmessages and other compiler in hbridge2 codesourcery

--- src/CanBusInterface.hpp
+++ src/CanBusInterface.hpp
@@ -4,34 +4,32 @@
 #include "Protocol.hpp"
 #include "canmessage.hh"
 #include "canbus.hh"
-#include <iostream>
 namespace hbridge {
 
-class CanBusInterface: public BusInterface
-{
+    class CanBusInterface: public BusInterface
+    {
 
-public:
-    CanBusInterface(canbus::Interface *interface);
-    virtual uint16_t getMaxPacketSize();
-    virtual bool readPacket(Packet& packet);
-    virtual bool sendPacket(const hbridge::Packet& packet);
+        public:
+            CanBusInterface(canbus::Interface *interface);
+            virtual uint16_t getMaxPacketSize();
+            virtual bool readPacket(Packet& packet);
+            virtual bool sendPacket(const hbridge::Packet& packet);
 
 
-    virtual bool readCanMsg(canbus::Message& msg){
-        assert(interface);
-        return interface->readCanMsg(msg);
+            virtual bool readCanMsg(canbus::Message& msg){
+                assert(interface);
+                return interface->readCanMsg(msg);
+            };
+
+            virtual bool sendCanMsg(const canbus::Message &msg){
+                assert(interface);
+                return interface->sendCanMsg(msg);
+            }
+
+        private:
+            canbus::Interface *interface;
+
     };
-
-    virtual bool sendCanMsg(const canbus::Message &msg){
-	std::cout << "SENDED CAN MSG ID" << msg.can_id << std::endl;
-        assert(interface);
-        return interface->sendCanMsg(msg);
-    }
-
-private:
-    canbus::Interface *interface;
-
-};
 
 }
 
