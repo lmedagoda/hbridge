223ca1482cc428edb808e01cf3c5c3ce09fd703d
Janosch Machowinski <scotch@imoby1.dfki.uni-bremen.de>
2013-03-01 16:36:36 +0100

    firmware: Can enable automatic re-transmit.
    If this option is disables we loos messages if the bus
    is temporally full.

--- firmware/hbridgeCommon/drivers/can.c
+++ firmware/hbridgeCommon/drivers/can.c
@@ -86,7 +86,7 @@ void CAN_Configuration(enum CAN_REMAP remap)
     CAN_InitStructure.CAN_TTCM=DISABLE;
     CAN_InitStructure.CAN_ABOM=ENABLE;
     CAN_InitStructure.CAN_AWUM=DISABLE;
-    CAN_InitStructure.CAN_NART=ENABLE;
+    CAN_InitStructure.CAN_NART=DISABLE;
     CAN_InitStructure.CAN_RFLM=DISABLE;
     CAN_InitStructure.CAN_TXFP=ENABLE;
     CAN_InitStructure.CAN_Mode=CAN_Mode_Normal;
