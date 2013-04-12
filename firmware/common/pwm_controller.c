427c0ed4a3894ca96dd2c6233fdef129b5f98641
Niklas Pech <npech@tzi.de>
2013-04-12 02:06:47 +0200

    Insert include limits.h and edit CMakelists

--- firmware/common/pwm_controller.c
+++ firmware/common/pwm_controller.c
@@ -24,6 +24,6 @@ void pwmControllerReset(int32_t wheelPos)
 int32_t pwmControllerStep(struct ControllerTargetData *targetData, int32_t wheelPos, uint32_t ticksPerTurn)
 {
     int16_t *pwmValue = (int16_t *) targetData->data;
-    printf("Setting pwm value %hi\n", *pwmValue);
+    //printf("Setting pwm value %hi\n", *pwmValue);
     return *pwmValue;
 }
