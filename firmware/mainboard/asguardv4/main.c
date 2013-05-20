87f67be8f6db5e81dc18057812912a3e98d7df94
Jakob Schwendner <jakob.schwendner@dfki.de>
2013-05-20 18:44:38 +0200

    fixed asguardv4 mainboard
    - asguard needs to send status packets, so that the ocu sends commands
    - hbridges were not configured correctly
    - PWM range has changed
    - status package sends a lot of dummy values

--- firmware/mainboard/asguardv4/main.c
+++ firmware/mainboard/asguardv4/main.c
@@ -14,7 +14,24 @@
 #include "../../hbridgeCommon/drivers/printf.h"
 #include <stddef.h>
 
+int16_t voltage_reading = 0;
+int16_t encoder_reading = 0;
 
+void sendStatusPacket() {
+    arc_packet_t packet;
+    packet.originator = SLAVE;
+    packet.system_id = 1; // ASGUARD
+    packet.packet_id = MB_STATUS;
+    packet.length = 7;
+    packet.data[0] = voltage_reading >> 8;
+    packet.data[1] = voltage_reading & 0xff;
+    packet.data[2] = mbstate_getCurrentState();
+    packet.data[3] = 0; //stat_packets_persec;
+    packet.data[4] = 0; //stat_control_packets_persec;
+    packet.data[5] = encoder_reading >> 8;
+    packet.data[6] = encoder_reading & 0xff;
+    arc_sendPacketDirect(&packet);
+}
 
 struct AsguardControlData {
   int8_t speed; 
@@ -35,9 +52,17 @@ void asguard_cmdHandler(int senderId, int receiverId, int id, unsigned char *dat
     cmdValid = 1;
 }
 
-#define SPEED_FACTOR 314/100
-#define DIRECTION_RADIUS 6/1000
-#define TURN_SPEED 20/100  
+#define STATUS_PACKET_PERIOD 500 
+#define SPEED_FACTOR (314)
+#define DIRECTION_RADIUS (6)
+#define TURN_SPEED (40)
+#define MAX_PWM 32767
+
+uint8_t asguard_enterRunningState(void)
+{
+    cmdValid = 0;
+    return mbstate_toRunningHandler();
+}
 
 /**
  * Handler that gets called if in running state
@@ -63,12 +88,16 @@ void asguard_runningState(void)
 
     
     //set actuator cmd
-    int left = curCmd.speed * SPEED_FACTOR *
-	(1000+1000*curCmd.direction*DIRECTION_RADIUS)/1000 + curCmd.turn *TURN_SPEED;
+    int left = curCmd.speed * SPEED_FACTOR + curCmd.turn *TURN_SPEED;
 
-    int right = curCmd.speed * SPEED_FACTOR * 
-	(1000-1000*curCmd.direction*DIRECTION_RADIUS)/1000 - curCmd.turn *TURN_SPEED;
-	
+    int right = curCmd.speed * SPEED_FACTOR - curCmd.turn *TURN_SPEED;
+
+    if(left > MAX_PWM)
+	left = MAX_PWM;
+
+    if(right > MAX_PWM)
+	right = MAX_PWM;
+
     hbridge_setValue(-left, right, right, -left);
 }
 
@@ -109,9 +138,18 @@ int main()
     hbridge_setControllerWithData(1, CONTROLLER_MODE_PWM, 0, NULL, 0);
     hbridge_setControllerWithData(2, CONTROLLER_MODE_PWM, 0, NULL, 0);
     hbridge_setControllerWithData(3, CONTROLLER_MODE_PWM, 0, NULL, 0);
+    int i = 0;
+    for(i = 0; i < 4; i++)
+    {
+	struct actuatorConfig *conf = getActuatorConfig(i);
+	conf->maxBoardTemp = 100;
+	conf->maxBoardTempCount = 100;
+	conf->maxCurrent = 4000;
+	conf->maxCurrentCount = 200;
+	conf->pwmStepPerMs = 1500;
+	conf->timeout = 150;
+    }
     
-    
-//     arc_init(USART1_SendData, USART1_GetData);
     
     arc_init(&USART1_SendData, &USART1_GetData, &USART1_SeekData);
     mbstate_init();
@@ -121,6 +159,7 @@ int main()
  
     ///Overload the state handler for running
     struct MainboardState *state=mbstate_getState(MAINBOARD_RUNNING);
+    state->enterStateHandler = asguard_enterRunningState;
     state->stateHandler=asguard_runningState;
     
     while(1)
@@ -138,15 +177,18 @@ int main()
 // 	    printf(".");
 	}
 
-	//process arc packets
-// 	arc_processPackets();
-	
 	arc_packet_t packet;	
-	
 	while(arc_readPacket(&packet))
 	{
 	    //process incomming packet
 	    packet_handlePacket(packet.originator, packet.system_id, packet.packet_id, packet.data, packet.length);	
+
+	    // respond with a status packet
+	    sendStatusPacket();
+
+	    // reset timeout 
+	    // TODO this should happen in the ping handler, but doesn't seem to be
+	    timeout_reset();
 	}
 	
 	//process hbridge driver
