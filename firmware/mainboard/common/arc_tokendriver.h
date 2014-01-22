9c47eebab26bb358152ebf4ef995be03e23c70ff
Niklas Pech <npech@tzi.de>
2014-01-22 01:33:40 +0100

    repairing broken arc_driver

---
+++ firmware/mainboard/common/arc_tokendriver.h
@@ -0,0 +1,12 @@
+#ifndef ARC_TOKENDRIVER_H
+#define ARC_TOKENDRIVER_H
+#include "arc_packet.h"
+#include "arc_driver.h"
+void arctoken_init(arc_send_func_t sendFunc, arc_recv_func_t recvFunc, arc_seek_func_t seekFunc);
+int arctoken_readPacket(arc_packet_t* packet);
+int arctoken_sendPacket(arc_packet_t* packet);
+void arctoken_processPackets();
+void arctoken_sendProtocolPacket(ARC_PACKET_ID id);
+void arctoken_setOwnSystemID(ARC_SYSTEM_ID sys_id);
+ARC_SYSTEM_ID arctoken_getOwnSystemID();
+#endif
