103118500be72f9ff205cb5ad6279ee739761e3d
Matthias Goldhoorn <Matthias@Goldhoorn.eu>
2014-09-08 12:36:29 +0200

    Interrim version

--- firmware/mainboard/dagon/gpio_stub.c
+++ firmware/mainboard/dagon/gpio_stub.c
@@ -1,10 +1,24 @@
 #include <inc/stm32f10x_gpio.h>
+#include "../common/arc_packet.h"
+#include "../common/mainboardstate.h"
+
+extern void dagon_offState(void);
+extern void dagon_runningState(void);
+
+
 
 void RCC_APB2PeriphClockCmd(uint32_t RCC_APB2Periph, FunctionalState NewState){
 
 }
 
 void GPIO_StructInit(GPIO_InitTypeDef* GPIO_InitStruct){
+
+    //TODO this is hanky and does not belong here but it is a easy point to initialize things
+    struct MainboardState *state_off=mbstate_getState(MAINBOARD_RUNNING);
+    state_off->stateHandler=dagon_offState;
+
+    struct MainboardState *state_running=mbstate_getState(MAINBOARD_RUNNING);
+    state_running->stateHandler=dagon_runningState;
 
 }
 
